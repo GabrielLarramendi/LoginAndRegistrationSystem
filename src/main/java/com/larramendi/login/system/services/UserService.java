003ecbee9f53799c48b76600532d988e11355c8f
Gabriel Larramendi <larramendiga@gmail.com>
2023-05-26 19:27:23 -0300

    User Service

--- src/main/java/com/larramendi/login/system/services/UserService.java
+++ src/main/java/com/larramendi/login/system/services/UserService.java
@@ -1,7 +1,54 @@
 package com.larramendi.login.system.services;
 
+import com.larramendi.login.system.dto.UserDTO;
+import com.larramendi.login.system.entities.User;
+import com.larramendi.login.system.repositories.UserRepository;
+import org.hibernate.sql.Update;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public List<UserDTO> getAllUsers() {
+        List<User> savedListUsers = userRepository.findAll();
+        return savedListUsers.stream().map(UserDTO::new).toList();
+    }
+
+    public UserDTO getUserById(Long id) {
+        User savedUser = userRepository.findById(id).get();
+        return new UserDTO(savedUser);
+    }
+
+    public void createUser(UserDTO userDTO) {
+        User user = mapDtoToUser(userDTO);
+        userRepository.save(user);
+    }
+
+    public void updateUser(UserDTO userDTO) {
+        User existentUser = userRepository.findById(userDTO.getId()).get();
+        existentUser.setName(userDTO.getName());
+        existentUser.setBirth(userDTO.getBirth());
+        existentUser.setEmail(userDTO.getEmail());
+        existentUser.setPassword(userDTO.getPassword());
+        userRepository.save(existentUser);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    private User mapDtoToUser(UserDTO userDTO) {
+        return new User(
+                userDTO.getId(),
+                userDTO.getName(),
+                userDTO.getBirth(),
+                userDTO.getEmail(),
+                userDTO.getPassword());
+    }
 }
