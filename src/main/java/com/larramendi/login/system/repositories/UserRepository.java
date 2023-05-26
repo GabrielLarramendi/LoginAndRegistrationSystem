3dd1867ca61aa4e42342c7bd75937ca85fe04302
Gabriel Larramendi <larramendiga@gmail.com>
2023-05-26 18:31:22 -0300

    User repository

---
+++ src/main/java/com/larramendi/login/system/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.larramendi.login.system.repositories;
+
+import com.larramendi.login.system.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
