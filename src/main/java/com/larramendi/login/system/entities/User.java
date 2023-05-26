6cdc733350feea3d29cd14a686a580abba995bb0
Gabriel Larramendi <larramendiga@gmail.com>
2023-05-26 18:30:50 -0300

    User entity

---
+++ src/main/java/com/larramendi/login/system/entities/User.java
@@ -0,0 +1,27 @@
+package com.larramendi.login.system.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Date birth;
+    private String email;
+    private String password;
+
+}
