e72dc779007351c850c8f0ceba148488297458ac
Gabriel Larramendi <larramendiga@gmail.com>
2023-06-13 23:06:43 -0300

    Form controller and Register page + delete button

--- src/main/java/com/larramendi/login/system/controllers/PagesController.java
+++ src/main/java/com/larramendi/login/system/controllers/PagesController.java
@@ -3,17 +3,16 @@ package com.larramendi.login.system.controllers;
 import com.larramendi.login.system.dto.UserDTO;
 import com.larramendi.login.system.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 @Controller
-public class HelloWorldController {
+public class PagesController {
 
     @Autowired
     UserService userService;
@@ -30,5 +29,11 @@ public class HelloWorldController {
         UserDTO savedUser = userService.getUserById(id);
         model.addAttribute("user", savedUser);
         return "user";
+    }
+
+    @DeleteMapping("user/{id}")
+    public String deleteUserById(Model model, @PathVariable Long id) {
+        userService.deleteUser(id);
+        return "redirect:/users";
     }
 }
